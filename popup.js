72e669c18a1f768e499569889336c092ebc4ad07
cwoodland <cwoodland@microsoft.com>
2022-10-02 21:19:16 -0400

    made code simplier

--- popup.js
+++ popup.js
@@ -1,22 +1,14 @@
-// Initialize button with user's preferred color
-let makeLink = document.getElementById("makeLink");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-makeLink.addEventListener("click", async () => {
-  let tab = await getCurrentTab();
-  console.log(`Tab ID:`, tab.id);
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      console.log(`Inside button click`);
-      //DO WORK HERE
-    },
-  });
-});
+
+async function createLinks() {
+  alert('HEY MAN!')
+}
 
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  return tab.id || -1;
 }
+
+document.getElementById('makeLink').addEventListener('click', createLinks);
